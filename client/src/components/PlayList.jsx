8942f911137ea0afb731b17d5769910f66fae3ca
fxg <fx.nippon@gmail.com>
2019-03-02 12:02:24 +0100

    10th commit: UserSelectedSongs component added, minor changes to PlayList.jsx

--- client/src/components/PlayList.jsx
+++ client/src/components/PlayList.jsx
@@ -1,21 +1,36 @@
 import React, { Component } from 'react'
 import ListItemPlaylist from './ListItemPlaylist';
+import UserSelectedSongs from './UserSelectedSongs';
 
 export default class PlayList extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      songsInPlaylist: [],
+      userSelectedSongs: [],
+      songsInPlaylist: [], 
     }
   }
+
+  componentDidMount() {
+  }
+
   render() {
     return (
-      <div className="playlist">
-          <ul>
-            {this.state.songsInPlaylist.map((song, i) => 
-              <ListItemPlaylist title={song.title} artist={song.artist} img={song} songId={song.id} key={i} />
-            )}
-          </ul>
+      <div>
+        <div className="userSongs">
+            <ul>
+              {this.state.songsInPlaylist.map((song, i) => 
+                <UserSelectedSongs title={song.title} artist={song.artist} img={song} songId={song.id} key={i} />
+              )}
+            </ul>
+        </div>
+        <div className="playlist">
+            <ul>
+              {this.state.songsInPlaylist.map((song, i) => 
+                <ListItemPlaylist title={song.title} artist={song.artist} img={song} songId={song.id} key={i} />
+              )}
+            </ul>
+        </div>
       </div>
     )
   }
