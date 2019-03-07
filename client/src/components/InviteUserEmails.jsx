bb52696a2f7946444aab8e752860285e5960598d
fxg <fx.nippon@gmail.com>
2019-03-07 14:24:14 +0100

    23rd commit: minor fixes for deployment + changes to InviteUserEmails component

--- client/src/components/InviteUserEmails.jsx
+++ client/src/components/InviteUserEmails.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
-import { InputGroup, InputGroupAddon, Button, Input } from 'reactstrap';
+import { ListGroup, ListGroupItem, Button } from 'reactstrap';
 
 export default function inviteUserEmails(props) {
   return (
     <div id='invited-user-emails-container'>
       {props.emails.map((email, i) => 
-        <InputGroup className='email-input' key={i} >
-          <Input value={email} />
-          <InputGroupAddon addonType="append">
-            <Button size='sm'>Edit</Button>
-          </InputGroupAddon>
-          <InputGroupAddon addonType="append">
-            <Button size='sm'>—</Button>
-          </InputGroupAddon>
-        </InputGroup>
+        <ListGroup id='invited-user-emails-list' className='email-input' >
+          <ListGroupItem id='invited-user-emails-list-item' key={i}>{email}<Button color='secondary'>—</Button></ListGroupItem>
+        </ListGroup>
       )}
     </div>
   )
