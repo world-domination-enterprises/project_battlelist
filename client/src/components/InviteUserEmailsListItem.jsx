e4ba250ec6bdf7b3dd5e28f44737c56a6ae59b56
Petter <petter.astedt@gmail.com>
2019-03-08 09:40:11 +0100

    last minute styling

--- client/src/components/InviteUserEmailsListItem.jsx
+++ client/src/components/InviteUserEmailsListItem.jsx
@@ -5,15 +5,16 @@ export default function InviteUserEmailsListItem(props) {
   return (
     // TODO --- IMPORTANT: this needs to be styled
     <div id='invited-user-emails-container'>
-      <ListGroup id='invited-user-emails-list' className='email-input' >
+      <ListGroup id='invited-user-emails-list' className='email-input col-4 mx-auto p-0 m-0' >
         {props.emails.map((email, i) => 
           <ListGroupItem 
+           className="invited-user-emails-list-item"
             id='invited-user-emails-list-item' 
             key={i}>{email}
             <Button 
-              color='secondary'
+              className="btn btn-danger m-2"               
               onClick={(i) => props.onEmailDelete(i)}
-              >—</Button>
+              >-</Button>
           </ListGroupItem>)}
       </ListGroup>
     </div>
--- client/src/components/pages/LandingPage.jsx
+++ client/src/components/pages/LandingPage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function LandingPage() {
   return (
-    <div className='landing-page-container'>
-      <h1><p id='landing-title'><strong><span>listr.</span></strong></p> is shared playlist creation in real time. Login with Spotify, invite your friends and let the fun begin.</h1>
+    <div className='landing-page-container d-flex flex-column'>
+      <h1>LISTR.</h1><p id='landing-title'>Is shared playlist creation in real time.<br /> Login with Spotify, <strong>invite your friends</strong> and let the fun begin.</p>
     </div>
   )
 }
